4a85d78d9c7d28abdec1d0228fb4a4104b3af87f
Anna Wang <annawang@annas-imac.attlocal.net>
2019-12-04 21:26:46 -0600

    Fix links

--- js/HandlebarsCode/galleryCode.js
+++ js/HandlebarsCode/galleryCode.js
@@ -1,9 +1,9 @@
 $.ajax({
    type: 'GET',
-   url: './data/sampleDataV7.json',
+   url: '../data/sampleDataV7.json',
    dataType: 'json',
    success: function( data ) {
-      
+
       $(document).ready( function() {
       // Gallery items build
       var thumbnailSource = $('#galleryTemplate').html();
